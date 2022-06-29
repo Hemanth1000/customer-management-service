601d71ea88cef5954739b66ab27b6ea10cbbb2f5
Hemanth Reddy Loka <lokahemanthreddy@gmail.com>
2022-06-29 13:23:45 -0500

    created a new API for updating customer details

--- src/main/java/customer/management/service/resources/CustomerController.java
+++ src/main/java/customer/management/service/resources/CustomerController.java
@@ -3,15 +3,19 @@ package customer.management.service.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import customer.management.service.adapter.Customer;
 import customer.management.service.adapter.CustomerRepository;
+import customer.management.service.exceptions.ResourceNotFoundException;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -29,5 +33,22 @@ public class CustomerController {
 	@PostMapping("/customers")
 	public Customer createCustomer(@RequestBody Customer customer) {
 		return customerRepository.save(customer);
+	}
+	
+	@GetMapping("/customers/{id}")
+	public ResponseEntity<Customer> getCustomerById(@PathVariable long id) {
+		Customer customer = customerRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Customer not found with id: "+ id));
+		return ResponseEntity.ok(customer);
+	}
+	
+	@PutMapping("/customers/{id}")
+	public ResponseEntity<Customer> updateCustomer(@PathVariable long id, @RequestBody Customer customerDetails){
+		Customer customer = customerRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Customer not found with id : "+id));
+		customer.setFirstName(customerDetails.getFirstName());
+		customer.setLastName(customerDetails.getLastName());
+		customer.setEmailId(customerDetails.getEmailId());
+		return ResponseEntity.ok(customerRepository.save(customer));
 	}
 }
